52b0d01f17d3b1ab7d7a05405e7eaae5ddb63a59
agungrizkyana <agung.rizkyana@gmail.com>
2022-05-14 11:34:26 +0700

    list post with simple modal

--- src/pages/list-post.jsx
+++ src/pages/list-post.jsx
@@ -1,7 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useListPostDispatcher } from "../redux/reducers/list-post";
 
+const Modal = ({ post }) => {
+  return (
+    <div className="fixed w-full left-0 bottom-0">
+      <div className="mx-auto max-w-lg border py-3">
+        <div className="font-bold 2xl">{post.title}</div>
+        <div>{post.caption}</div>
+      </div>
+    </div>
+  );
+};
+
 const ListPostPage = () => {
+  const [singlePost, setSinglePost] = useState();
+
   const {
     listPost: { posts },
     loadPosts,
@@ -20,7 +33,12 @@ const ListPostPage = () => {
           <div className="flex justify-between w-full">
             <div className="font-bold">{postItem.attributes.title}</div>
             <div className="">
-              <button type="button">edit</button>
+              <button
+                type="button"
+                onClick={() => setSinglePost(postItem.attributes)}
+              >
+                edit
+              </button>
             </div>
           </div>
         </div>
@@ -31,6 +49,8 @@ const ListPostPage = () => {
   return (
     <div className="max-w-lg min-h-screen pt-6 pb-8 mx-auto">
       <div className="flex flex-col w-full space-y-3">{renderPostItems()}</div>
+      {singlePost && singlePost.title && <Modal post={singlePost} />}
+      {/* {JSON.stringify(singlePost)} */}
     </div>
   );
 };
